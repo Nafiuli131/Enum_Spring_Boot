22ac23969338d2b892d0dec8c8ef6dd270b7ab18
Nafiul-Islam-Nayeem <nafiul.islam@digigate360.com>
2023-05-18 17:34:57 +0600

    add enum code

---
+++ Enum/src/main/java/com/example/Enum/Entity/Employee.java
@@ -0,0 +1,20 @@
+package com.example.Enum.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
---
+++ Enum/src/main/java/com/example/Enum/Enum/Designation.java
@@ -0,0 +1,7 @@
+package com.example.Enum.Enum;
+
+public enum Designation {
+    JUNIOR_SOFTWARE_ENGINEER,
+    SOFTWARE_ENGINEER,
+    SENIOR_SOFTWARE_ENGINEER;
+}
