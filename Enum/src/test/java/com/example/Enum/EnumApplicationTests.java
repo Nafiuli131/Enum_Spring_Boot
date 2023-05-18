89141a04de394ac3a5153d9c3d9c47b1b574b464
Nafiul-Islam-Nayeem <nafiul.islam@digigate360.com>
2023-05-18 16:19:17 +0600

    structure of this app

---
+++ Enum/src/test/java/com/example/Enum/EnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Enum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
